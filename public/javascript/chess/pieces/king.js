974005cec41604531fb45bad798f4b6d5094852a
Bishal <bishal.space@gmail.com>
2024-09-26 23:24:38 +0530

    Update king.js

--- public/javascript/chess/pieces/king.js
+++ public/javascript/chess/pieces/king.js
@@ -1,38 +1,40 @@
-var King = function(config) {
+var King = function(config){
     this.type = 'king';
     this.constructor(config);
 };
 
 King.prototype = new Piece({});
 
-King.prototype.isValid = function (targetPosition) {
+King.prototype.isValidPosition = function(newPosition) {
     let currentCol = this.position.charAt(0);
     let currentRow = parseInt(this.position.charAt(1));
-    let targetCol = targetPosition.col;
-    let targetRow = parseInt(targetPosition.row);
-    let targetPiece = this.board.getPieceAt(targetPosition);
+    
+    let targetCol = newPosition.col;
+    let targetRow = parseInt(newPosition.row);
 
-    if (Math.abs(targetCol.charCodeAt(0) - currentCol.charCodeAt(0)) <= 1 && Math.abs(targetRow - currentRow) <= 1) {
-        if (targetPiece && targetPiece.color === this.color) {
-            return false;
+    let colDiff = Math.abs(targetCol.charCodeAt(0) - currentCol.charCodeAt(0));
+    let rowDiff = Math.abs(targetRow - currentRow);
+
+    // King can move one square in any direction
+    if (colDiff <= 1 && rowDiff <= 1) {
+        let targetPiece = this.board.getPieceAt(newPosition);
+        if (!targetPiece || targetPiece.color !== this.color) {
+            return true;
         }
-
-        if (targetPiece && targetPiece.color !== this.color) {
-            targetPiece.kill(targetPiece);
-        }
-
-        return true;
     }
 
+    console.warn("Invalid move for king");
     return false;
 };
 
 King.prototype.moveTo = function(newPosition) {
-    if (this.isValid(newPosition)) {
+    if (this.isValidPosition(newPosition)) {
+        let targetPiece = this.board.getPieceAt(newPosition);
+        if (targetPiece && targetPiece.color !== this.color) {
+            this.kill(targetPiece);
+        }
         this.position = newPosition.col + newPosition.row;
         this.render();
-        this.board.switchPlayer();
-    } else {
-        this.board.invalidMove();
+        this.board.switchTurn();
     }
 };
